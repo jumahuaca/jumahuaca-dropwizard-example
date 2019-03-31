b37a0c55b7478b01e0df741f342d34f1778ec701
Marcos Forlenza <mnforlenza@gmail.com>
2019-03-31 00:02:40 -0300

    Integration test utility classes added. Refactor

--- src/test/java/org/jumahuaca/examples/jdbc/dao/JdbcExchangeDaoImplIntegrationTests.java
+++ src/test/java/org/jumahuaca/examples/jdbc/dao/JdbcExchangeDaoImplIntegrationTests.java
@@ -3,21 +3,11 @@ package org.jumahuaca.examples.jdbc.dao;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.io.File;
 import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
 
-import javax.sql.DataSource;
-
-import org.dbunit.IDatabaseTester;
-import org.dbunit.JdbcDatabaseTester;
-import org.dbunit.dataset.IDataSet;
-import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
-import org.dbunit.operation.DatabaseOperation;
-import org.h2.jdbcx.JdbcDataSource;
-import org.h2.tools.RunScript;
 import org.jumahuaca.examples.exceptions.NotFoundException;
 import org.jumahuaca.examples.exceptions.ServerErrorException;
 import org.jumahuaca.examples.jdbc.model.UVAExchange;
@@ -26,13 +16,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-public class JdbcExchangeDaoImplIntegrationTests {
-
-	private static final String JDBC_DRIVER = org.h2.Driver.class.getName();
-	private static final String JDBC_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
-	private static final String USER = "sa";
-	private static final String PASSWORD = "";
-
+public class JdbcExchangeDaoImplIntegrationTests extends JdbcCrudDaoIntegrationTests{
+	
 	private static final String SCHEMA_FILE_NAME = "test-schema.sql";
 	private static final String MODEL_FILE_NAME = "test-model.xml";
 
@@ -58,34 +43,14 @@ public class JdbcExchangeDaoImplIntegrationTests {
 
 	@BeforeAll
 	public static void createSchema() throws Exception {
-		RunScript.execute(JDBC_URL, USER, PASSWORD, SCHEMA_FILE_NAME, null, false);
+		JdbcCrudDaoIntegrationTests.createSchema(SCHEMA_FILE_NAME);
 	}
 
 	@BeforeEach
 	public void importDataSet() throws Exception {
-		IDataSet dataSet = readDataSet();
-		cleanlyInsert(dataSet);
+		prepareDataset(MODEL_FILE_NAME);
 		dao = new UvaExchangeDaoImpl(buildDataSource());
 		daoInvocation = new JdbcExchangeDaoInvocation(dao);
-	}
-
-	private DataSource buildDataSource() {
-		JdbcDataSource dataSource = new JdbcDataSource();
-		dataSource.setURL(JDBC_URL);
-		dataSource.setUser(USER);
-		dataSource.setPassword(PASSWORD);
-		return dataSource;
-	}
-
-	private IDataSet readDataSet() throws Exception {
-		return new FlatXmlDataSetBuilder().build(new File(MODEL_FILE_NAME));
-	}
-
-	private void cleanlyInsert(IDataSet dataSet) throws Exception {
-		IDatabaseTester databaseTester = new JdbcDatabaseTester(JDBC_DRIVER, JDBC_URL, USER, PASSWORD);
-		databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
-		databaseTester.setDataSet(dataSet);
-		databaseTester.onSetup();
 	}
 
 	@Test
